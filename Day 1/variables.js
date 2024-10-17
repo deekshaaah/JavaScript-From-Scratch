350734c86d10ca9bfc35aa01a462b0f76b341676
Deeksha Singh <145819105+deekshaaah@users.noreply.github.com>
2024-10-17 12:33:00 +0530

    Create variables.js

---
+++ Day 1/variables.js
@@ -0,0 +1,24 @@
+// defining a variable
+var number1 = 5;
+number1 = 3;
+
+console.log(number1); // 3
+window.alert(number1); // 3
+
+//mathematical operations
+number1 = number1 + 5; // 3 + 5 = 8
+number1 = number1 - 6; // 8 - 6 = 2
+var number2 = number1 * 10; // 2 (times) 10 = 20
+var number3 = number2 / number1; // 20 (divided by) 2 = 10;
+
+//concatenation
+var myString = "Hello!" + " I am Shasha";
+
+// array
+var favoriteFruits = ["apple", "orange", "strawberry"];
+window.alert(favoriteFruits[0]); // accessing
+
+//objects
+john = {firstname: "John", lastname: "Doe", fullname: "John Doe"};
+window.alert(john.fullname); // John Doe // accessing
+
